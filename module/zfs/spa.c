e2065e91eadb96ae6273594df444699a155390cb
Matt Macy <mmacy@FreeBSD.org>
2018-12-04 00:39:12 +0000

    ZoF integration 7/10

--- module/zfs/spa.c
+++ module/zfs/spa.c
@@ -2400,7 +2400,7 @@ spa_load(spa_t *spa, spa_load_state_t state, spa_import_type_t type)
 	return (error);
 }
 
-#ifdef ZFS_DEBUG
+#if defined(ZFS_DEBUG) && !defined(NDEBUG)
 /*
  * Count the number of per-vdev ZAPs associated with all of the vdevs in the
  * vdev tree rooted in the given vd, and ensure that each ZAP is present in the

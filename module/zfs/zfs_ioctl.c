83b8c3979e1ecfa6ce81226ec6993eca290b61a5
Matt Macy <mmacy@FreeBSD.org>
2019-01-20 00:22:31 +0000

    do FreeBSD specific zvol minor creation

--- module/zfs/zfs_ioctl.c
+++ module/zfs/zfs_ioctl.c
@@ -3418,6 +3418,10 @@ zfs_ioc_create(const char *fsname, nvlist_t *innvl, nvlist_t *outnvl)
 			}
 		}
 	}
+#if defined(__FreeBSD__) && defined(_KERNEL)
+	if (error == 0 && type == DMU_OST_ZVOL)
+		zvol_create_minors(NULL, fsname, B_TRUE);
+#endif
 	return (error);
 }
 
@@ -3466,6 +3470,10 @@ zfs_ioc_clone(const char *fsname, nvlist_t *innvl, nvlist_t *outnvl)
 		if (error != 0)
 			(void) dsl_destroy_head(fsname);
 	}
+#if defined(__FreeBSD__) && defined(_KERNEL)
+	if (error == 0)
+		zvol_create_minors(NULL, fsname, B_TRUE);
+#endif
 	return (error);
 }
 
@@ -4894,6 +4902,11 @@ zfs_ioc_recv_impl(char *tofs, char *tosnap, char *origin, nvlist_t *recvprops,
 		zfs_ioc_recv_inject_err = B_FALSE;
 		error = 1;
 	}
+#endif
+
+#if defined(__FreeBSD__) && defined(_KERNEL)
+	if (error == 0)
+		zvol_create_minors(NULL, tofs, B_TRUE);
 #endif
 
 	/*

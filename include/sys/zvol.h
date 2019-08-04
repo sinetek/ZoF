2236ae727da3b7d82a62b93c2c73c96bd9280ced
Matthew Macy <mmacy@FreeBSD.org>
2019-08-04 15:34:34 -0700

    add platform specific bits for FreeBSD - but don't add to build yet

--- include/sys/zvol.h
+++ include/sys/zvol.h
@@ -46,7 +46,6 @@ typedef struct zvol_state zvol_state_t;
 extern int zvol_check_volsize(uint64_t volsize, uint64_t blocksize);
 extern int zvol_check_volblocksize(const char *name, uint64_t volblocksize);
 extern int zvol_get_stats(objset_t *os, nvlist_t *nv);
-extern boolean_t zvol_is_zvol(const char *);
 extern void zvol_create_cb(objset_t *os, void *arg, cred_t *cr, dmu_tx_t *tx);
 extern int zvol_set_volsize(const char *, uint64_t);
 extern int zvol_set_volblocksize(const char *, uint64_t);
@@ -59,5 +58,11 @@ extern void *zvol_tag(zvol_state_t *);
 extern int zvol_init(void);
 extern void zvol_fini(void);
 extern int zvol_busy(void);
+
+/*
+ * Platform dependent
+ */
+extern boolean_t zvol_is_zvol(const char *);
+
 #endif /* _KERNEL */
 #endif /* _SYS_ZVOL_H */

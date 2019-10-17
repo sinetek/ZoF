c8b11b14a9e5bd2bbd617312b696bd11f8e0660f
Ryan Moeller <ryan@iXsystems.com>
2019-10-17 15:57:55 +0000

    Fix zpool import on FreeBSD
    We needed to prepend /dev/ to device names in order for the import scan
    to be able to open them. The path gets stripped again elsewhere, which
    probably needs to be checked to make sure paths like "gpt/$label" work.

--- lib/libzfs/libzfs_pool.c
+++ lib/libzfs/libzfs_pool.c
@@ -3925,7 +3925,6 @@ zpool_vdev_name(libzfs_handle_t *hdl, zpool_handle_t *zhp, nvlist_t *nv,
 			path = zfs_strip_path(path);
 		}
 
-#ifndef __FreeBSD__
 		/*
 		 * Remove the partition from the path it this is a whole disk.
 		 */
@@ -3933,7 +3932,6 @@ zpool_vdev_name(libzfs_handle_t *hdl, zpool_handle_t *zhp, nvlist_t *nv,
 		    == 0 && value && !(name_flags & VDEV_NAME_PATH)) {
 			return (zfs_strip_partition(path));
 		}
-#endif
 	} else {
 		path = type;
 

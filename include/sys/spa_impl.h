3e8ccf3e13d7a696265083043504b9509e894e44
Matt Macy <mmacy@FreeBSD.org>
2019-10-25 20:47:26 +0000

    enable procfs_list usage on FreeBSD

--- include/sys/spa_impl.h
+++ include/sys/spa_impl.h
@@ -448,8 +448,6 @@ extern void spa_load_l2cache(spa_t *spa);
 extern sysevent_t *spa_event_create(spa_t *spa, vdev_t *vd, nvlist_t *hist_nvl,
     const char *name);
 extern void spa_event_post(sysevent_t *ev);
-extern void spa_import_progress_init(void);
-extern void spa_import_progress_destroy(void);
 extern int param_set_deadman_failmode_common(const char *val);
 
 #ifdef	__cplusplus

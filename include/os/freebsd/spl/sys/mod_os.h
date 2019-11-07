d8b7dacb7a75c2a876cca5ab01056ebc98cacd8f
Matt Macy <mmacy@FreeBSD.org>
2019-11-07 00:23:39 +0000

    fix build post-rebase

--- include/os/freebsd/spl/sys/mod_os.h
+++ include/os/freebsd/spl/sys/mod_os.h
@@ -8,4 +8,22 @@
 
 #define ZFS_MODULE_PARAM_CALL(scope_prefix, name_prefix, name, setfunc, getfunc, perm, desc)
 
+#include <sys/kernel.h>
+#define module_init(fn)							\
+static void \
+wrap_ ## fn(void *dummy __unused) \
+{								 \
+	fn();						 \
+}																		\
+ SYSINIT(zfs_ ## fn, SI_SUB_LAST, SI_ORDER_FIRST, wrap_ ## fn, NULL)
+
+
+#define module_exit(fn) 							\
+static void \
+wrap_ ## fn(void *dummy __unused) \
+{								 \
+	fn();						 \
+}																		\
+SYSUNINIT(zfs_ ## fn, SI_SUB_LAST, SI_ORDER_FIRST, wrap_ ## fn, NULL)
+
 #endif /* SPL_MOD_H */

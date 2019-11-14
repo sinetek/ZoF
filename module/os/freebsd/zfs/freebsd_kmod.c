1536ea21b101751ef1ebcf2b3bc0d7d503928715
Allan Jude <allanjude@freebsd.org>
2019-11-14 00:29:00 +0000

    Trigger spa_boot_init() at mountroot
    This is required for pools to import at boot
    
    Signed-off-by: Allan Jude <allanjude@freebsd.org>

--- module/os/freebsd/zfs/freebsd_kmod.c
+++ module/os/freebsd/zfs/freebsd_kmod.c
@@ -387,6 +387,11 @@ static moduledata_t zfs_mod = {
 	zfs_modevent,
 	0
 };
+
+#ifdef _KERNEL
+EVENTHANDLER_DEFINE(mountroot, spa_boot_init, NULL, 0);
+#endif
+
 DECLARE_MODULE(zfsctrl, zfs_mod, SI_SUB_CLOCKS, SI_ORDER_ANY);
 MODULE_VERSION(zfsctrl, 1);
 MODULE_DEPEND(zfsctrl, krpc, 1, 1, 1);

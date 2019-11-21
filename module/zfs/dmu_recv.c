460f70bd384a7678515a3cc93de38a27da089b6f
Matthew Macy <mmacy@FreeBSD.org>
2019-11-21 12:51:07 -0800

    whitespace reverts

--- module/zfs/dmu_recv.c
+++ module/zfs/dmu_recv.c
@@ -1196,6 +1196,7 @@ dmu_recv_begin(char *tofs, char *tosnap, dmu_replay_record_t *drr_begin,
 			dsl_crypto_params_free(drba.drba_dcp, !!err);
 		}
 	}
+
 	if (err != 0) {
 		kmem_free(drc->drc_next_rrd, sizeof (*drc->drc_next_rrd));
 		nvlist_free(drc->drc_begin_nvl);

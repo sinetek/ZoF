78fda3e29512dc26c40d28edba524a0db1affaf3
Matt Macy <mmacy@FreeBSD.org>
2019-11-27 22:44:54 +0000

    fix user build

--- lib/libspl/include/os/freebsd/sys/param.h
+++ lib/libspl/include/os/freebsd/sys/param.h
@@ -61,9 +61,10 @@ extern size_t spl_pagesize(void);
 
 extern int execvpe(const char *name, char * const argv[], char * const envp[]);
 
+struct zfs_handle;
 /*
  * Attach/detach the given filesystem to/from the given jail.
  */
-extern int zfs_jail(zfs_handle_t *zhp, int jailid, int attach);
+extern int zfs_jail(struct zfs_handle *zhp, int jailid, int attach);
 
 #endif

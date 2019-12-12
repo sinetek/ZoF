f140f39b0e3e7b6d9286726388e4a75898ba874f
Matt Macy <mmacy@FreeBSD.org>
2019-12-12 00:27:41 +0000

    update vnops

--- include/os/freebsd/zfs/sys/zfs_vnops.h
+++ include/os/freebsd/zfs/sys/zfs_vnops.h
@@ -4,25 +4,25 @@ int dmu_write_pages(objset_t *os, uint64_t object, uint64_t offset,
     uint64_t size, struct vm_page **ppa, dmu_tx_t *tx);
 int dmu_read_pages(objset_t *os, uint64_t object, vm_page_t *ma, int count,
     int *rbehind, int *rahead, int last_size);
-extern int zfs_remove(vnode_t *dip, char *name, cred_t *cr, int flags);
-extern int zfs_mkdir(vnode_t *dip, char *dirname, vattr_t *vap,
-    vnode_t **ipp, cred_t *cr, int flags, vsecattr_t *vsecp);
-extern int zfs_rmdir(vnode_t *dip, char *name, vnode_t *cwd,
+extern int zfs_remove(znode_t *dzp, char *name, cred_t *cr, int flags);
+extern int zfs_mkdir(znode_t *dzp, char *dirname, vattr_t *vap,
+    znode_t **zpp, cred_t *cr, int flags, vsecattr_t *vsecp);
+extern int zfs_rmdir(znode_t *dzp, char *name, znode_t *cwd,
     cred_t *cr, int flags);
-extern int zfs_setattr(vnode_t *ip, vattr_t *vap, int flag, cred_t *cr);
-extern int zfs_rename(vnode_t *sdip, char *snm, vnode_t *tdip,
+extern int zfs_setattr(znode_t *zp, vattr_t *vap, int flag, cred_t *cr);
+extern int zfs_rename(znode_t *sdzp, char *snm, znode_t *tdzp,
     char *tnm, cred_t *cr, int flags);
-extern int zfs_symlink(vnode_t *dip, const char *name, vattr_t *vap,
-    const char *link, vnode_t **ipp, cred_t *cr, int flags);
-extern int zfs_link(vnode_t *tdip, vnode_t *sip,
+extern int zfs_symlink(znode_t *dzp, const char *name, vattr_t *vap,
+    const char *link, znode_t **zpp, cred_t *cr, int flags);
+extern int zfs_link(znode_t *tdzp, znode_t *sp,
     char *name, cred_t *cr, int flags);
-extern int zfs_space(vnode_t *ip, int cmd, struct flock *bfp, int flag,
+extern int zfs_space(znode_t *zp, int cmd, struct flock *bfp, int flag,
     offset_t offset, cred_t *cr);
-extern int zfs_create(vnode_t *dip, char *name, vattr_t *vap, int excl,
-    int mode, vnode_t **ipp, cred_t *cr, int flag, vsecattr_t *vsecp);
-extern int zfs_setsecattr(vnode_t *ip, vsecattr_t *vsecp, int flag,
+extern int zfs_create(znode_t *dzp, char *name, vattr_t *vap, int excl,
+    int mode, znode_t **zpp, cred_t *cr, int flag, vsecattr_t *vsecp);
+extern int zfs_setsecattr(znode_t *zp, vsecattr_t *vsecp, int flag,
     cred_t *cr);
 ssize_t
-zpl_write_common(vnode_t *ip, char *buf, size_t len, loff_t *ppos,
+zpl_write_common(vnode_t *vp, char *buf, size_t len, loff_t *ppos,
     uio_seg_t segment, int flags, cred_t *cr);
 #endif
